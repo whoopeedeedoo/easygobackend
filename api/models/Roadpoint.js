93c85661e4befa69d6eb6b98b88ad560484e84ab
ballchen <kerkerball@gmail.com>
2014-07-26 00:45:58 +0800

    把PROXY改成我們NODE用的 增加ROADPOINT MODEL

---
+++ api/models/Roadpoint.js
@@ -0,0 +1,17 @@
+/**
+ * Roadpoint.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+		id: String,
+		name: String,
+		latitude: String,
+		longitude: String,
+		time: Date
+	}
+};
