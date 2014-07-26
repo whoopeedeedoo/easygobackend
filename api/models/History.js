4056e13c5fb3ce0f2b814b3b2a19176255424a2e
ballchen <kerkerball@gmail.com>
2014-07-26 12:37:14 +0800

    history

--- api/models/History.js
+++ api/models/History.js
@@ -7,17 +7,10 @@
 
 module.exports = {
 
+	connection: 'mongo',
 	attributes: {
-		start: 'STRING',
-		end: 'STRING'
+		path: [],
+		userid: 'string'
 
 	}
 };
-
-
-起點說明文字 string
-起點座標 string
-終點說明文字 string
-終點座標 string
-各路點說明文字 string
-各路點做標 string
